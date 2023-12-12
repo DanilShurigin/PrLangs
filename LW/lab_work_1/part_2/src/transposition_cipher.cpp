06ab2caab31bc40371223a8bfe620df8c192dfb1
DanilShurigin <=>
2023-12-12 10:00:14 +0300

    LW1_complite

--- LW/lab_work_1/part_2/src/transposition_cipher.cpp
+++ LW/lab_work_1/part_2/src/transposition_cipher.cpp
@@ -10,7 +10,7 @@ std::string TranspositionCipher::encrypt(const std::string& message) {
   int rows = on_table_length / key_;
 
   for (int i=0; i<message_length; ++i) {
-    int trp_i = rows * (key_ - 1 - (i % key_)) + (i / key_);
+    int trp_i = rows * (key_ - (1 + (i % key_))) + (i / key_);
     encrypted_message.at(trp_i) = message.at(i);
   }
   return encrypted_message;
@@ -18,14 +18,17 @@ std::string TranspositionCipher::encrypt(const std::string& message) {
 
 std::string TranspositionCipher::decrypt(const std::string& message) {
   int message_length = message.size();
-  int on_table_length = (message_length/key_ + 1) * key_;
+  int on_table_length = (message_length/key_ + (message_length % key_ != 0)) * key_;
 
-  std::string decrypted_message(on_table_length, '-');
+  std::string decrypted_message(on_table_length, ' ');
 
   int rows = on_table_length / key_;
 
   for (int i=0; i<message_length; ++i) {
-    int trp_i = rows * ((i % key_) + 1) - ((i / key_) + 1);
+    int trp_i = key_ * (i % rows) + key_ - ((i / rows) + 1);
+    if (message.at(i) == '-') {
+      continue;
+    }
     decrypted_message.at(trp_i) = message.at(i);
   }
   return decrypted_message;
